be50596a92875f8d42466fcd6a5a161571fd9366
joaoAngelo2 <joaoangeloribeironunes@gmail.com>
2026-05-29 18:07:43 -0300

    feat: criado o header

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,19 @@
+import Header from "./components/Header"
+
+function App() {
+
+
+  return (
+    <>
+      <header className="w-full h-16 bg-red-500">
+         <nav>
+          <ul>
+          </ul>
+         </nav>
+      
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/Header.tsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+  return (
+    <>
+    <div className="w-full h-16 bg-red-500">
+        
+      
+    </div>
+    </>
+  )
+}
+
+export default Header;
